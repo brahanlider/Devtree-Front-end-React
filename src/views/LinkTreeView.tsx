764fdd5d0a64d740d3bcf64a9c75c9f31f2d1bb2
brahanlider <brahanlidertunquipamamani@gmail.com>
2025-03-22 11:54:59 -0500

    Add: orden a los enalces Links

--- src/views/LinkTreeView.tsx
+++ src/views/LinkTreeView.tsx
@@ -41,14 +41,9 @@ export default function LinkTreeView() {
       link.name === e.target.name ? { ...link, url: e.target.value } : link
     );
     setDevTreeLinks(updateLinks);
-
-    queryClient.setQueryData(["user"], (prevData: TUser) => {
-      return {
-        ...prevData,
-        links: JSON.stringify(updateLinks),
-      };
-    });
   };
+
+  const links: TSocialNetwork[] = JSON.parse(user.links);
 
   const handleEnableLink = (socialNetwork: string) => {
     const updateEnableLink = devTreeLinks.map((link) => {
@@ -63,15 +58,62 @@ export default function LinkTreeView() {
     });
     setDevTreeLinks(updateEnableLink);
 
+    let updatedItems: TSocialNetwork[] = [];
+    const selectedSocialNetwork = updateEnableLink.find(
+      (link) => link.name === socialNetwork
+    );
+    if (selectedSocialNetwork?.enabled) {
+      const id = links.filter((link) => link.id).length + 1;
+      if (links.some((link) => link.name === socialNetwork)) {
+        updatedItems = links.map((link) => {
+          if (link.name === socialNetwork) {
+            return {
+              ...link,
+              enabled: true,
+              id,
+            };
+          } else {
+            return link;
+          }
+        });
+      } else {
+        const newItem = {
+          ...selectedSocialNetwork,
+          id,
+        };
+        updatedItems = [...links, newItem];
+      }
+    } else {
+      const indexToUpdate = links.findIndex(
+        (link) => link.name === socialNetwork
+      );
+      updatedItems = links.map((link) => {
+        if (link.name === socialNetwork) {
+          return {
+            ...link,
+            id: 0,
+            enabled: false,
+          };
+        } else if (link.id > indexToUpdate) {
+          return {
+            ...link,
+            id: link.id - 1,
+          };
+        } else {
+          return link;
+        }
+      });
+    }
+
+    // Almacenar en la base de datos
     queryClient.setQueryData(["user"], (prevData: TUser) => {
       return {
         ...prevData,
-        links: JSON.stringify(updateEnableLink),
+        links: JSON.stringify(updatedItems),
       };
     });
   };
 
-  // console.log(devTreeLinks);
   return (
     <div className="space-y-5">
       {devTreeLinks.map((item) => (
@@ -91,3 +133,5 @@ export default function LinkTreeView() {
     </div>
   );
 }
+
+// Faltar arreglar la logica para ordenar los id

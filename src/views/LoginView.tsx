3e347fe018a1e7a933ecbc5b1093ec0df1965191
brahanlider <brahanlidertunquipamamani@gmail.com>
2025-03-22 19:47:40 -0500

    Add: navigate login auth go links

--- src/views/LoginView.tsx
+++ src/views/LoginView.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 import ErrorMessage from "../components/ErrorMessage";
 import { LoginForm } from "../types";
@@ -7,6 +7,8 @@ import { toast } from "sonner";
 import { isAxiosError } from "axios";
 
 export default function LoginView() {
+  const navigate = useNavigate();
+
   const initialValues: LoginForm = {
     email: "",
     password: "",
@@ -22,6 +24,7 @@ export default function LoginView() {
     try {
       const { data } = await api.post(`/auth/login`, formData);
       localStorage.setItem("AUTH_TOKEN", data);
+      navigate("/admin");
     } catch (error) {
       if (isAxiosError(error) && error.response) {
         toast.error(error.response.data.error);
